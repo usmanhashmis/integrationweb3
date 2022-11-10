e541c619e7aa5711087e546518fbbd9afd115f0c
Ali Mohsin <91325231+alimohsinz@users.noreply.github.com>
2022-11-10 20:56:16 +0500

    first commit

--- App.js
+++ App.js
@@ -1,35 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React, {useEffect} from 'react';
-
+import React from 'react';
 import './global';
 import './shim';
-
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
 import Depositassets from './Depositassets';
-
-//hashmi
-///usman hasnededejheebhfebhfh
-
-const App: () => Node = () => {
-
-  return (
-    <Depositassets/>
-  );
+//alimohsin
+const App = () => {
+  return <Depositassets />;
 };
 
 export default App;
